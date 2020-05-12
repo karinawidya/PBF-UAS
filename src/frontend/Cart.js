151b974bbd658df563a7ea768f278e70e59a3398
karinawidya <karinawidya9999@gmail.com>
2020-05-12 22:51:10 +0700

    update cart page

--- src/frontend/Cart.js
+++ src/frontend/Cart.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import firebase from '../Firebase';
 
 export class Cart extends Component {
+    constructor() {
+        super();;
+        this.state = {
+            produk:'',
+            qty:''
+        };
+    }
+
+    onChange = (e) => {
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const { produk, qty } = this.state;
+
+        firebase.database().ref("/Report Summary").push().set(this.state);
+        this.props.history.push("/")
+    }
+
     render() {
+        const { produk, qty } = this.state;
         return (
             <div>
                 <Navbar/>
+                <br />
+               
                 <h2 className="mt-5 d-flex justify-content-center">
                 Tidak ada produk yang ditambahkan
                 </h2>
                 <h3 className="mt-5 d-flex justify-content-center">
-                0 produk </h3>
+                Yuk, tambahkan produk ke cart Anda! </h3>
+                <form class="col-sm-6 mx-auto" onSubmit={this.onSubmit}>
+                <div class="form-group">
+                        <input type="text" class="form-control" id="produk" name="produk" value={produk} onChange={this.onChange} placeholder="Input Product" />
+                    </div>
+                    <div class="form-group">
+                        <input type="number" class="form-control" id="qty" name="qty" value={qty} onChange={this.onChange} placeholder="Input Your Qty" />
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                    </form>
             </div>
         )
     }
